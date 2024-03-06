b61d334fbcfe8ab339759fe36a242df651c1aa8f
Ravi Shankar Mehta <nitravi.m@gmail.com>
2024-03-06 17:20:47 +0530

    implemented pipe validation and error

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import * as mongoose from 'mongoose';
 import { Book } from './schemas/book.schema';
@@ -10,6 +10,7 @@ export class BookService {
     @InjectModel(Book.name)
     private bookModel: mongoose.Model<Book>,
   ) {}
+
   async findAll(query: Query): Promise<Book[]> {
     const resPerPage = 2;
     const currentPage = Number(query.page) || 1;
@@ -28,23 +29,32 @@ export class BookService {
       .skip(skip);
     return books;
   }
+
   async create(book: Book): Promise<Book> {
     const res = await this.bookModel.create(book);
     return res;
   }
+
   async findById(id: string): Promise<Book> {
+
+    const isValidId = mongoose.isValidObjectId(id)
+    if (!isValidId) {
+        throw new BadRequestException('Please enter correct id.');
+      }
     const book = await this.bookModel.findById(id);
     if (!book) {
       throw new NotFoundException('Book not found.');
     }
     return book;
   }
+
   async updateById(id: string, book: Book): Promise<Book> {
     return await this.bookModel.findByIdAndUpdate(id, book, {
       new: true,
       runValidators: true,
     });
   }
+
   async deleteById(id: string): Promise<Book> {
     return await this.bookModel.findByIdAndDelete(id);
   }
